5780765544bb12047c9b91710c540472ae79e0af
Iker <iachagaro22wg@ikzubirimanteo.com>
2024-09-23 13:01:18 +0200

    bukatuta

--- src/ariketaForm/form.php
+++ src/ariketaForm/form.php
@@ -3,13 +3,21 @@
 if (isset($_POST['bidali'])) {
 
     if (isset($_POST['izena']) && !empty($_POST['izena'])) {
-
-        echo "Izena: " . $_POST['izena'] . "<br>";
+        if (strlen($_POST['izena']) <16) {
+            echo "Izena: " . $_POST['izena'] . "<br>";
+        } else {
+            echo "Izena luzegia da";
+        }
+        
     }
 
     if (isset($_POST['deskripzioa']) && !empty($_POST['deskripzioa'])) {
-
-        echo "Deskripzioa: " . $_POST['deskripzioa'] . "<br>";
+        if (strlen($_POST['deskripzioa']) < 50) {
+            echo "Deskripzioa: " . $_POST['deskripzioa'] . "<br>";
+        }else {
+            echo "deskripzioa luzegia da.";
+        }
+        
     }
 
     if (isset($_POST['generoa']) && !empty($_POST['generoa'])) {
@@ -18,22 +26,26 @@ if (isset($_POST['bidali'])) {
     }
 
     if (isset($_POST['adina']) && !empty($_POST['adina'])) {
-
         echo "Adina: " . $_POST['adina'] . "<br>";
     }
 
     if (isset($_POST['email']) && !empty($_POST['email'])) {
-
-        echo "Email: " . $_POST['email'] . "<br>";
+        if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            echo "Email: " . $_POST['email'] . "<br>";
+        } else {
+            echo "Email-a ez du formatu egokia";
+        }
+        
     }
 
     if (isset($_POST['argazkia']) && !empty($_POST['argazkia'])) {
-        echo "<img src='/src/ariketaForm/irudiak/" . $_POST['argazkia'] . "'>" . "<br>";
+        echo "Argazkia: <img src='irudiak/" . $_POST['argazkia'] . "' " .  "width='200' height='200'>" . "<br>";
     }
     
     
     if (isset($_POST['password']) && !empty($_POST['password'])) {
-
-        echo "Pasahitza: " . $_POST['password'] . "<br>";
+        if (strlen($_POST['password']) >= 8 && 16 < strlen($_POST['password'])) {
+            echo "Pasahitza: " . $_POST['password'] . "<br>";
+        } 
     }
 }
