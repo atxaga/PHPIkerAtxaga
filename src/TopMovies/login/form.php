1ba431d372e8430e4453bb91b2c20d58b0890b61
Iker <iachagaro22wg@ikzubirimanteo.com>
2024-10-18 10:44:44 +0200

    Bukatuta

--- src/TopMovies/login/form.php
+++ src/TopMovies/login/form.php
@@ -11,21 +11,41 @@ $_SESSION['erabiltzaileaId'] = $erabiltzailea1;
 $erabiltzailea = $_POST['erabiltzailea'];
 $pasahitza = $_POST['pasahitza'];
 
+
+
 $selectErabiltzaileSQL = "SELECT id FROM erabiltzaileak ORDER BY id DESC LIMIT 1";
 $result = $conn->query($selectErabiltzaileSQL);
 $azkenErabil = $result->fetch_assoc();
-
 $id = $azkenErabil['id'] + 1; 
 
-$sql = "INSERT INTO erabiltzaileak (id, izena, pasahitza)
-        VALUES ('$id', '$erabiltzailea', '$pasahitza')";
-        
-if ($conn->query($sql) === TRUE) {
-    header("Location: ../index.php");
-    exit();
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+$selectErabiltzaileGuztiakSql = "SELECT * FROM erabiltzaileak";
+$result = $conn->query($selectErabiltzaileGuztiakSql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        if ($row['izena'] == $_POST['erabiltzailea']) {
+            echo "Dagoeneko erabiltzaile hori erregistratuta dago";
+        } else {
+            insertErabiltzaile();
+        }
+    }
 }
+
+
+function insertErabiltzaile() {
+    global $conn, $id, $erabiltzailea, $pasahitza;
+
+    $sql = "INSERT INTO erabiltzaileak (id, izena, pasahitza) VALUES ('$id', '$erabiltzailea', '$pasahitza')";
+        
+            if ($conn->query($sql) === TRUE) {
+                header("Location: ../index.php");
+                exit();
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+}
+
+
 
 function get_Erabiltzailea() {
     global $conn;
